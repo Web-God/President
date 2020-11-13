9e9a4f0c870a7af5dcd89b2fd8192a008120875f
Web-God <alvardsandz@gmail.com>
2020-11-13 16:55:23 +0100

    modify custom.js add data-card

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,27 +1,30 @@
 // @ts-nocheck
 // @ts-expect-error
 //@ts-check
-document.addEventListener("DOMContentLoaded", function (event) {
-  'use strict';
-  try {
+document.addEventListener("DOMContentLoaded", function(event) {
+    'use strict';
+    try {
 
-  } catch (error) {
+    } catch (error) {
 
-  }
-  let cardBack = document.querySelectorAll('.mask');
-  let timeFront;
-// Add class to see tips of game on the back of the card
-  function flipCard() {
-    this.classList.add('back');
-  }
-  // Remove class to see the mask
-  timeFront = setInterval(() => {
-    for (let i = 0; i < cardBack.length; i++) {
-      const el = cardBack[i];
-      el.classList.remove('back');
     }
-  }, 10000);
-  cardBack.forEach(function (mask) {
-    mask.addEventListener('click', flipCard);
-  });
+    let cardBack = document.querySelectorAll('.mask');
+    let timeFront;
+    // Add class to see tips of game on the back of the card
+    function flipCard() {
+        this.classList.add('back');
+    }
+    // Remove class to see the mask
+    timeFront = setInterval(() => {
+        for (let i = 0; i < cardBack.length; i++) {
+            const el = cardBack[i];
+            el.classList.remove('back');
+        }
+    }, 10000);
+    cardBack.forEach(function(mask) {
+        mask.addEventListener('click', flipCard);
+    });
 });
+
+//Identify tips cards
+let dataTips = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
